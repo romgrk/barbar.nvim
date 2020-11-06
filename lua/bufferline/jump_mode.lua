bc2bfc1629dac0ef4b3f8e70ac966cc7f4e65f86
Rom Grk <romgrk.cc@gmail.com>
2020-11-06 17:39:44 -0500

    fix: issues with jump-mode

--- lua/bufferline/jump_mode.lua
+++ lua/bufferline/jump_mode.lua
@@ -21,25 +21,27 @@ local bufname = vim.fn.bufname
 local LETTERS = vim.g.bufferline.letters
 local INDEX_BY_LETTER = {}
 
-local letter_status = {} -- array
-local buffer_by_letter = {} -- object
-local letter_by_buffer = {} -- object
+local m = {
+  letter_status = {}, -- array
+  buffer_by_letter = {}, -- object
+  letter_by_buffer = {}, -- object
+}
 
 -- Initialize INDEX_BY_LETTER
 for index = 1, len(LETTERS) do
   local letter = slice(LETTERS, index, index)
   INDEX_BY_LETTER[letter] = index
-  letter_status[index] = false
+  m.letter_status[index] = false
 end
 
 -- local empty_bufnr = nvim.create_buf(0, 1)
 
 local function assign_next_letter(bufnr)
-  if letter_by_buffer[bufnr] ~= nil then
+  bufnr = tonumber(bufnr)
+
+  if m.letter_by_buffer[bufnr] ~= nil then
     return
   end
-
-  bufnr = tonumber(bufnr)
 
   -- First, try to assign a letter based on name
   if vim.g.bufferline.semantic_letters == true then
@@ -50,12 +52,12 @@ local function assign_next_letter(bufnr)
 
       if INDEX_BY_LETTER[letter] ~= nil then
         local index = INDEX_BY_LETTER[letter]
-        local status = letter_status[index]
+        local status = m.letter_status[index]
         if status == false then
-          letter_status[index] = true
+          m.letter_status[index] = true
           -- letter = LETTERS[index]
-          buffer_by_letter[letter] = bufnr
-          letter_by_buffer[bufnr] = letter
+          m.buffer_by_letter[letter] = bufnr
+          m.letter_by_buffer[bufnr] = letter
           return letter
         end
       end
@@ -63,12 +65,12 @@ local function assign_next_letter(bufnr)
   end
 
   -- Otherwise, assign a letter by usable order
-  for i, status in ipairs(letter_status) do
+  for i, status in ipairs(m.letter_status) do
     if status == 0 then
       local letter = LETTERS[i]
-      letter_status[i] = true
-      buffer_by_letter[letter] = bufnr
-      letter_by_buffer[bufnr] = letter
+      m.letter_status[i] = true
+      m.buffer_by_letter[letter] = bufnr
+      m.letter_by_buffer[bufnr] = letter
       return letter
     end
   end
@@ -83,18 +85,18 @@ local function unassign_letter(letter)
 
   local index = INDEX_BY_LETTER[letter]
 
-  letter_status[index] = false
+  m.letter_status[index] = false
 
-  if buffer_by_letter[letter] ~= nil then
-    local bufnr = buffer_by_letter[letter]
-    buffer_by_letter[letter] = nil
-    letter_by_buffer[bufnr] = nil
+  if m.buffer_by_letter[letter] ~= nil then
+    local bufnr = m.buffer_by_letter[letter]
+    m.buffer_by_letter[letter] = nil
+    m.letter_by_buffer[bufnr] = nil
   end
 end
 
 local function get_letter(bufnr)
-   if letter_by_buffer[bufnr] ~= nil then
-      return letter_by_buffer[bufnr]
+   if m.letter_by_buffer[bufnr] ~= nil then
+      return m.letter_by_buffer[bufnr]
    end
    return assign_next_letter(bufnr)
 end
@@ -111,15 +113,15 @@ end
 --     if letter_from_buffer == nil or assigned_letters[letter_from_buffer] ~= nil then
 --         letter_from_buffer = assign_next_letter(bufnr)
 --     else
---         letter_status[index] = true
+--         m.letter_status[index] = true
 --     end
 --     if letter_from_buffer ~= nil then
---         let bufnr_from_state = get(s:buffer_by_letter, letter_from_buffer, nil)
+--         let bufnr_from_state = get(s:m.buffer_by_letter, letter_from_buffer, nil)
 -- 
 --         if bufnr_from_state ~= bufnr
---           let s:buffer_by_letter[letter_from_buffer] = bufnr
---           if has_key(s:buffer_by_letter, bufnr_from_state)
---               call remove(s:buffer_by_letter, bufnr_from_state)
+--           let s:m.buffer_by_letter[letter_from_buffer] = bufnr
+--           if has_key(s:m.buffer_by_letter, bufnr_from_state)
+--               call remove(s:m.buffer_by_letter, bufnr_from_state)
 --           end
 --         end
 -- 
@@ -130,7 +132,7 @@ end
 --   let index = 0
 --   for index in range(len(s:LETTERS))
 --     let letter = s:LETTERS[index]
---     let status = s:letter_status[index]
+--     let status = s:m.letter_status[index]
 --     if status && !has_key(assigned_letters, letter)
 --         call s:unassign_letter(letter)
 --     end
@@ -139,9 +141,9 @@ end
 
 -- print(vim.inspect(get_letter(nvim.get_current_buf())))
 -- print(vim.inspect(get_letter(nvim.get_current_buf())))
--- print(vim.inspect(letter_status))
+-- print(vim.inspect(m.letter_status))
 -- print(vim.inspect(unassign_letter('j')))
--- print(vim.inspect(letter_status))
+-- print(vim.inspect(m.letter_status))
 
 -- 
 -- local function shadow_open()
@@ -184,8 +186,8 @@ local function activate()
   local did_switch = false
 
   if letter ~= '' then
-    if buffer_by_letter[letter] ~= nil then
-      local bufnr = buffer_by_letter[letter]
+    if m.buffer_by_letter[letter] ~= nil then
+      local bufnr = m.buffer_by_letter[letter]
       nvim.command('buffer' .. bufnr)
       did_switch = true
     else
@@ -195,16 +197,13 @@ local function activate()
     end
   end
 
-  if not did_switch then
-    vim.fn['bufferline#update']()
-    nvim.command('redraw')
-  end
+  vim.fn['bufferline#update']()
+  nvim.command('redraw')
 end
 
-return {
-  activate = activate,
-  get_letter = get_letter,
-  unassign_letter = unassign_letter,
-  unassign_letter_for = unassign_letter_for,
-  assign_next_letter = assign_next_letter,
-}
+m.activate = activate
+m.get_letter = get_letter
+m.unassign_letter = unassign_letter
+m.unassign_letter_for = unassign_letter_for
+m.assign_next_letter = assign_next_letter
+return m
