d45b4e9f2d107f63e76ec66d7f397ab8906a93be
Rom Grk <romgrk.cc@gmail.com>
2020-11-06 20:16:42 -0500

    typo

--- lua/bufferline/get-icon.lua
+++ lua/bufferline/get-icon.lua
@@ -8,8 +8,8 @@ local status, web = pcall(require, 'nvim-web-devicons')
 local function get_icon(buffer_name, filetype)
   if status == false then
     nvim.command('echohl WarningMsg')
-    nvim.command('echom "babar: bufferline.icons is set to v:true but \\\"nvim-dev-icons\\\" was not found."')
-    nvim.command('echom "babar: icons have been disabled. Set bufferline.icons to v:false to disable this message."')
+    nvim.command('echom "barbar: bufferline.icons is set to v:true but \\\"nvim-dev-icons\\\" was not found."')
+    nvim.command('echom "barbar: icons have been disabled. Set bufferline.icons to v:false to disable this message."')
     nvim.command('echohl None')
     nvim.command('let g:bufferline.icons = v:false')
     return ' '
