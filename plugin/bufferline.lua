1eb16d4dd4c02a51d8507d1e0e337fd1e02d08ba
Iron-E <code.iron.e@gmail.com>
2022-05-10 15:40:40 -0400

    fix(plugin): broken commands

--- plugin/bufferline.lua
+++ plugin/bufferline.lua
@@ -4,139 +4,130 @@
 -- Date: Fri 22 May 2020 02:22:36 AM EDT
 -- !::exe [So]
 
+local bufferline = require 'bufferline'
+
 vim.opt.showtabline = 2
 
 --------------------
 -- Section: Commands
 --------------------
 
-vim.api.nvim_create_user_command('BarbarEnable', 'call bufferline#enable()', {description = 'Enable barbar.nvim'})
-vim.api.nvim_create_user_command('BarbarDisable', 'call bufferline#disable()', {description = 'Disable barbar.nvim'})
+vim.api.nvim_create_user_command('BarbarEnable', bufferline.enable, {desc = 'Enable barbar.nvim'})
+vim.api.nvim_create_user_command('BarbarDisable', bufferline.disable, {desc = 'Disable barbar.nvim'})
 
 vim.api.nvim_create_user_command(
   'BufferNext',
   function(tbl) require'bufferline.state'.goto_buffer_relative(math.max(tbl.count, 1)) end,
-  {bang = true, count = true, description = 'Go to the next buffer'}
+  {count = true, desc = 'Go to the next buffer'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferPrevious',
   function(tbl) require'bufferline.state'.goto_buffer_relative(-math.max(tbl.count, 1)) end,
-  {bang = true, count = true, description = 'Go to the previous buffer'}
+  {count = true, desc = 'Go to the previous buffer'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferGoto',
-  function(tbl) require'bufferline.state'.goto_buffer(tbl.fargs) end,
-  {nargs = 1, count = true, description = 'Go to the specified buffer'}
+  function(tbl) require'bufferline.state'.goto_buffer(tbl.args) end,
+  {desc = 'Go to the buffer at the specified index', nargs = 1}
 )
 
-vim.api.nvim_create_user_command(
-  'BufferLast',
-  function() require'bufferline.state'.goto_buffer(-1) end,
-  {count = true, description = 'Go to the last buffer'}
-)
+vim.api.nvim_create_user_command('BufferLast', '<Cmd>BufferGoto 1<CR>', {desc = 'Go to the first buffer'})
+vim.api.nvim_create_user_command('BufferLast', '<Cmd>BufferGoto -1<CR>', {desc = 'Go to the last buffer'})
 
 vim.api.nvim_create_user_command(
   'BufferMove',
-  function(tbl) require'bufferline.state'.move_current_buffer(-math.max(tbl.count, 1)) end,
-  {bang = true, nargs = 1, description = 'Move the current buffer to the left'}
+  function(tbl) vim.api.nvim_command('<Cmd>BufferMovePrevious ' .. tbl.count .. '<CR>') end,
+  {count = true, desc = 'Synonym for `:BufferMovePrevious`'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferMoveNext',
   function(tbl) require'bufferline.state'.move_current_buffer(math.max(tbl.count, 1)) end,
-  {count = true, description = 'Move the current buffer to the right'}
+  {count = true, desc = 'Move the current buffer to the right'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferMovePrevious',
   function(tbl) require'bufferline.state'.move_current_buffer(-math.max(tbl.count, 1)) end,
-  {count = true, description = 'Move the current buffer to the left'}
+  {count = true, desc = 'Move the current buffer to the left'}
 )
 
-vim.api.nvim_create_user_command(
-  'BufferPick',
-  function() require'bufferline.jump_mode'.activate() end,
-  {bang = true, description = 'Pick a buffer'}
-)
+vim.api.nvim_create_user_command('BufferPick', function() require'bufferline.jump_mode'.activate() end, {desc = 'Pick a buffer'})
 
-vim.api.nvim_create_user_command(
-  'BufferPin',
-  function() require'bufferline.state'.toggle_pin() end,
-  {description = 'Un/pin a buffer'}
-)
+vim.api.nvim_create_user_command('BufferPin', function() require'bufferline.state'.toggle_pin() end, {desc = 'Un/pin a buffer'})
 
 vim.api.nvim_create_user_command(
   'BufferOrderByBufferNumber',
   function() require'bufferline.state'.order_by_buffer_number() end,
-  {bang = true, description = 'Order the bufferline by buffer number'}
+  {desc = 'Order the bufferline by buffer number'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferOrderByDirectory',
   function() require'bufferline.state'.order_by_directory() end,
-  {bang = true, description = 'Order the bufferline by directory'}
+  {desc = 'Order the bufferline by directory'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferOrderByLanguage',
   function() require'bufferline.state'.order_by_language() end,
-  {bang = true, description = 'Order the bufferline by language'}
+  {desc = 'Order the bufferline by language'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferOrderByWindowNumber',
   function() require'bufferline.state'.order_by_window_number() end,
-  {bang = true, description = 'Order the bufferline by window number'}
+  {desc = 'Order the bufferline by window number'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferClose',
-  function(tbl) vim.fn['bufferline#bbye#delete']('bdelete', tostring(tbl.bang), tbl.args, tbl.mods) end,
-  {bang = true, complete = 'buffer', description = 'Close the current buffer.', nargs = '?'}
+  function(tbl) vim.fn['bufferline#bbye#delete']('bdelete', tbl.bang and '!' or '', tbl.args, tbl.mods) end,
+  {bang = true, complete = 'buffer', desc = 'Close the current buffer.', nargs = '?'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferDelete',
-  '<Cmd>BufferClose<CR>',
-  {bang = true, complete = 'buffer', description = 'Synonym for `:BufferClose`', nargs = '?'}
+  function(tbl) vim.fn['bufferline#bbye#delete']('bdelete', tbl.bang and '!' or '', tbl.args, tbl.mods) end,
+  {bang = true, complete = 'buffer', desc = 'Synonym for `:BufferClose`', nargs = '?'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferWipeout',
-  function(tbl) vim.fn['bufferline#bbye#delete']('bwipeout', tostring(tbl.bang), tbl.args, tbl.mods) end,
-  {bang = true, complete = 'buffer', description = 'Wipe out the buffer', nargs = '?'}
+  function(tbl) vim.fn['bufferline#bbye#delete']('bwipeout', tbl.bang and '!' or '', tbl.args, tbl.mods) end,
+  {bang = true, complete = 'buffer', desc = 'Wipe out the buffer', nargs = '?'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferCloseAllButCurrent',
   function() require'bufferline.state'.close_all_but_current() end,
-  {description = 'Close every buffer except the current one'}
+  {desc = 'Close every buffer except the current one'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferCloseAllButPinned',
   function() require'bufferline.state'.close_all_but_pinned() end,
-  {description = 'Close every buffer except pinned buffers'}
+  {desc = 'Close every buffer except pinned buffers'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferCloseAllButCurrentOrPinned',
   function() require'bufferline.state'.close_all_but_current_or_pinned() end,
-  {description = 'Close every buffer except pinned buffers or the current buffer'}
+  {desc = 'Close every buffer except pinned buffers or the current buffer'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferCloseBuffersLeft',
   function() require'bufferline.state'.close_buffers_left() end,
-  {description = 'Close all buffers to the left of the current buffer'}
+  {desc = 'Close all buffers to the left of the current buffer'}
 )
 
 vim.api.nvim_create_user_command(
   'BufferCloseBuffersRight',
   function() require'bufferline.state'.close_buffers_right() end,
-  {description = 'Close all buffers to the right of the current buffer'}
+  {desc = 'Close all buffers to the right of the current buffer'}
 )
 
 -------------------
@@ -175,7 +166,9 @@ vim.fn.dictwatcheradd(vim.g.bufferline, '*', 'v:lua.BufferlineOnOptionChanged')
 -- Section: Event handlers
 --------------------------
 
--- Needs to be global -_-
+--- What to do when `vim.g.bufferline` is changed.
+--- Needs to be global -_-
+--- @param key string what option was changed.
 function BufferlineOnOptionChanged(_, key, _)
   vim.g.bufferline = vim.tbl_extend('keep', vim.g.bufferline or {}, DEFAULT_OPTIONS)
   if key == 'letters' then
@@ -183,7 +176,10 @@ function BufferlineOnOptionChanged(_, key, _)
   end
 end
 
--- Needs to be global -_-
+--- What to do when clicking.
+--- Needs to be global -_-
+--- @param btn string
+--- @param minwid number
 function BufferlineMainClickHandler(minwid, _, btn, _)
   if minwid == 0 then
     return
@@ -197,12 +193,13 @@ function BufferlineMainClickHandler(minwid, _, btn, _)
   end
 end
 
--- Needs to be global -_-
+--- What to do when clicking the close button.
+--- Needs to be global -_-
+--- @param minwid number
 function BufferlineCloseClickHandler(minwid, _, _, _)
   vim.fn['bufferline#bbye#delete']('bdelete', '', minwid)
 end
 
 
 -- Final setup
-
-vim.fn['bufferline#enable']()
+bufferline.enable()
