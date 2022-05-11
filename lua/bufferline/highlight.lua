8b1b4d3b7c947f250769e954980382cf2b2ed039
Iron-E <code.iron.e@gmail.com>
2022-05-11 11:37:17 -0400

    fix(highlight): use `cterm` for `notermguicolors`

--- lua/bufferline/highlight.lua
+++ lua/bufferline/highlight.lua
@@ -9,9 +9,10 @@ local icons = require 'bufferline.icons'
 --- Generate a color.
 --- @param index string where to look for the color.
 --- @param groups table<string> the groups to source the color from.
---- @param default string the foreground color to use if no `groups` have a valid color.
---- @return string color
-local function color(index, groups, default)
+--- @param default string the color to use if no `groups` have a valid color.
+--- @param default_cterm number|string the color to use if no `groups` have a valid color and `termguicolors == false`.
+--- @return number|string color
+local function color(index, groups, default, default_cterm)
   local guicolors = vim.go.termguicolors
   for _, group in ipairs(groups) do
     local hl = vim.api.nvim_get_hl_by_name(group, guicolors)
@@ -19,23 +20,25 @@ local function color(index, groups, default)
       return guicolors and string.format('#%06x', hl[index]) or hl[index]
     end
   end
-  return default
+  return guicolors and default or default_cterm
 end
 
 --- Generate a foreground color.
 --- @param groups table<string> the groups to source the foreground color from.
 --- @param default string the foreground color to use if no `groups` have a valid foreground color.
---- @return string color
-local function fg(groups, default)
-  return color('foreground', groups, default)
+--- @param default_cterm number|string the color to use if no `groups` have a valid color and `termguicolors == false`.
+--- @return number|string color
+local function fg(groups, default, default_cterm)
+  return color('foreground', groups, default, default_cterm or default)
 end
 
 --- Generate a background color.
 --- @param groups table<string> the groups to source the background color from.
 --- @param default string the background color to use if no `groups` have a valid background color.
---- @return string color
-local function bg(groups, default)
-  return color('background', groups, default)
+--- @param default_cterm number|string the color to use if no `groups` have a valid color and `termguicolors == false`.
+--- @return number|string color
+local function bg(groups, default, default_cterm)
+  return color('background', groups, default, default_cterm or default)
 end
 
 ------------------
@@ -47,21 +50,19 @@ local highlight = {}
 
 -- Initialize highlights
 function highlight.setup()
-  local guicolors = vim.go.termguicolors
-
   local fg_target = 'red'
 
-  local fg_current  = fg({'Normal'}, guicolors and '#efefef' or 255)
-  local fg_visible  = fg({'TabLineSel'}, guicolors and '#efefef' or 255)
-  local fg_inactive = fg({'TabLineFill'}, guicolors and '#888888' or 102)
+  local fg_current  = fg({'Normal'}, '#efefef', 255)
+  local fg_visible  = fg({'TabLineSel'}, '#efefef', 255)
+  local fg_inactive = fg({'TabLineFill'}, '#888888', 102)
 
-  local fg_modified = fg({'WarningMsg'}, guicolors and '#E5AB0E' or 178)
-  local fg_special  = fg({'Special'}, guicolors and '#599eff' or 75)
-  local fg_subtle = fg({'NonText', 'Comment'}, guicolors and '#555555' or 240)
+  local fg_modified = fg({'WarningMsg'}, '#E5AB0E', 178)
+  local fg_special  = fg({'Special'}, '#599eff', 75)
+  local fg_subtle = fg({'NonText', 'Comment'}, '#555555', 240)
 
-  local bg_current  = bg({'Normal'}, 'none')
-  local bg_visible  = bg({'TabLineSel', 'Normal'}, 'none')
-  local bg_inactive = bg({'TabLineFill', 'StatusLine'}, 'none')
+  local bg_current  = bg({'Normal'}, 'none', nil)
+  local bg_visible  = bg({'TabLineSel', 'Normal'}, 'none', nil)
+  local bg_inactive = bg({'TabLineFill', 'StatusLine'}, 'none', nil)
 
   --      Current: current buffer
   --      Visible: visible but not current buffer
@@ -71,29 +72,46 @@ function highlight.setup()
   --         -Mod: when modified
   --        -Sign: the separator between buffers
   --      -Target: letter in buffer-picking mode
-  local hl = vim.api.nvim_set_hl
-  hl(0, 'BufferCurrent',        {bg = bg_current, fg = fg_current, default = true})
-  hl(0, 'BufferCurrentIndex',   {bg = bg_current, fg = fg_special, default = true})
-  hl(0, 'BufferCurrentMod',     {bg = bg_current, fg = fg_modified, default = true})
-  hl(0, 'BufferCurrentSign',    {bg = bg_current, fg = fg_special, default = true})
-  hl(0, 'BufferCurrentTarget',  {bg = bg_current, fg = fg_target, bold = true, default = true})
-  hl(0, 'BufferInactive',       {bg = bg_inactive, fg = fg_inactive, default = true})
-  hl(0, 'BufferInactiveIndex',  {bg = bg_inactive, fg = fg_subtle, default = true})
-  hl(0, 'BufferInactiveMod',    {bg = bg_inactive, fg = fg_modified, default = true})
-  hl(0, 'BufferInactiveSign',   {bg = bg_inactive, fg = fg_subtle, default = true})
-  hl(0, 'BufferInactiveTarget', {bg = bg_inactive, fg = fg_target, bold = true, default = true})
-  hl(0, 'BufferTabpageFill',    {bg = bg_inactive, fg = fg_inactive, default = true})
-  hl(0, 'BufferTabpages',       {bg = bg_inactive, fg = fg_special, bold = true, default = true})
-  hl(0, 'BufferVisible',        {bg = bg_visible, fg = fg_visible, default = true})
-  hl(0, 'BufferVisibleIndex',   {bg = bg_visible, fg = fg_visible, default = true})
-  hl(0, 'BufferVisibleMod',     {bg = bg_visible, fg = fg_modified, default = true})
-  hl(0, 'BufferVisibleSign',    {bg = bg_visible, fg = fg_visible, default = true})
-  hl(0, 'BufferVisibleTarget',  {bg = bg_visible, fg = fg_target, bold = true, default = true})
+  local hl = vim.go.termguicolors and
+    function(group, background, foreground, bold)
+      vim.api.nvim_set_hl(0, group, {
+        bg = background,
+        bold = bold,
+        default = true,
+        fg = foreground,
+      })
+    end or
+    function(group, background, foreground, bold)
+      vim.api.nvim_set_hl(0, group, {
+        bold = bold,
+        ctermbg = background,
+        ctermfg = foreground,
+        default = true,
+      })
+    end
 
-  hl(0, 'BufferCurrentIcon', {default = true, link = 'BufferCurrent'})
-  hl(0, 'BufferInactiveIcon', {default = true, link = 'BufferInactive'})
-  hl(0, 'BufferVisibleIcon', {default = true, link = 'BufferVisible'})
-  hl(0, 'BufferOffset', {default = true, link = 'BufferTabpageFill'})
+  hl('BufferCurrent',        bg_current, fg_current)
+  hl('BufferCurrentIndex',   bg_current, fg_special)
+  hl('BufferCurrentMod',     bg_current, fg_modified)
+  hl('BufferCurrentSign',    bg_current, fg_special)
+  hl('BufferCurrentTarget',  bg_current, fg_target, true)
+  hl('BufferInactive',       bg_inactive, fg_inactive)
+  hl('BufferInactiveIndex',  bg_inactive, fg_subtle)
+  hl('BufferInactiveMod',    bg_inactive, fg_modified)
+  hl('BufferInactiveSign',   bg_inactive, fg_subtle)
+  hl('BufferInactiveTarget', bg_inactive, fg_target, true)
+  hl('BufferTabpageFill',    bg_inactive, fg_inactive)
+  hl('BufferTabpages',       bg_inactive, fg_special, true)
+  hl('BufferVisible',        bg_visible, fg_visible)
+  hl('BufferVisibleIndex',   bg_visible, fg_visible)
+  hl('BufferVisibleMod',     bg_visible, fg_modified)
+  hl('BufferVisibleSign',    bg_visible, fg_visible)
+  hl('BufferVisibleTarget',  bg_visible, fg_target, true)
+
+  vim.api.nvim_set_hl(0, 'BufferCurrentIcon', {default = true, link = 'BufferCurrent'})
+  vim.api.nvim_set_hl(0, 'BufferInactiveIcon', {default = true, link = 'BufferInactive'})
+  vim.api.nvim_set_hl(0, 'BufferVisibleIcon', {default = true, link = 'BufferVisible'})
+  vim.api.nvim_set_hl(0, 'BufferOffset', {default = true, link = 'BufferTabpageFill'})
 
   icons.set_highlights()
 end
