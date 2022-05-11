3f81c08ff67345b4caaeefac038caf4a16c99f9f
Iron-E <code.iron.e@gmail.com>
2022-05-10 20:44:41 -0400

    perf(buffer): remove local globals; use API funcs

--- lua/bufferline/buffer.lua
+++ lua/bufferline/buffer.lua
@@ -3,22 +3,11 @@
 --
 
 
-local vim = vim
 local utils = require'bufferline.utils'
-local split = vim.split
-local join = table.concat
-local len = utils.len
-local slice = utils.slice
-local basename = utils.basename
-local bufname = vim.fn.bufname
-local bufwinnr = vim.fn.bufwinnr
-local matchlist = vim.fn.matchlist
-local nvim_buf_get_option = vim.api.nvim_buf_get_option
-local nvim_get_current_buf = vim.api.nvim_get_current_buf
 
 
 local function terminalname(name)
-  local result = matchlist(name, [===[term://.\{-}//\d\+:\(.*\)]===])
+  local result = vim.fn.matchlist(name, [===[term://.\{-}//\d\+:\(.*\)]===])
   if next(result) == nil then
     return name
   else
@@ -29,17 +18,17 @@ end
 
 -- returns 0: none, 1: active, 2: current
 local function get_activity(number)
-  if nvim_get_current_buf() == number then
+  if vim.api.nvim_get_current_buf() == number then
     return 2
   end
-  if bufwinnr(number) ~= -1 then
+  if vim.fn.bufwinnr(number) ~= -1 then
     return 1
   end
   return 0
 end
 
 local function get_name(opts, number)
-  local name = bufname(number)
+  local name = vim.api.nvim_buf_get_name(number)
 
   if name == '' then
     if opts.no_name_title ~= nil and
@@ -50,11 +39,11 @@ local function get_name(opts, number)
       name = '[buffer ' .. number .. ']'
     end
   else
-    local buftype = nvim_buf_get_option(number, 'buftype')
+    local buftype = vim.bo[number].buftype
     if buftype == 'terminal' then
       name = terminalname(name)
     else
-      name = basename(name)
+      name = utils.basename(name)
     end
   end
 
@@ -79,19 +68,19 @@ end
 
 local separator = package.config:sub(1,1)
 local function get_unique_name (first, second)
-  local first_parts  = split(first,  separator)
-  local second_parts = split(second, separator)
+  local first_parts  = vim.split(first,  separator)
+  local second_parts = vim.split(second, separator)
 
   local length = 1
-  local first_result  = join(slice(first_parts, -length),  separator)
-  local second_result = join(slice(second_parts, -length), separator)
+  local first_result  = table.concat(vim.list_slice(first_parts, -length),  separator)
+  local second_result = table.concat(vim.list_slice(second_parts, -length), separator)
 
   while first_result == second_result and
-        length < math.max(len(first_parts), len(second_parts))
+        length < math.max(utils.len(first_parts), utils.len(second_parts))
   do
     length = length + 1
-    first_result  = join(slice(first_parts,  -math.min(len(first_parts), length)),  separator)
-    second_result = join(slice(second_parts, -math.min(len(second_parts), length)), separator)
+    first_result  = table.concat(vim.list_slice(first_parts,  -math.min(utils.len(first_parts), length)),  separator)
+    second_result = table.concat(vim.list_slice(second_parts, -math.min(utils.len(second_parts), length)), separator)
   end
 
   return first_result, second_result
