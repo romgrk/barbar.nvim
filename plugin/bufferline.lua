1c753880be0b50389daed80ba94d022ad94765df
Iron-E <code.iron.e@gmail.com>
2023-03-29 18:40:43 -0400

    ref: deprecate `g:bufferline`

--- plugin/bufferline.lua
+++ plugin/bufferline.lua
@@ -4,4 +4,14 @@
 -- Date: Fri 22 May 2020 02:22:36 AM EDT
 -- !::exe [So]
 
-require'bufferline'.setup(vim.g.bufferline)
+local user_config = vim.g.bufferline
+
+if user_config then
+  require'bufferline.utils'.notify_once(
+    "`g:bufferline` is deprecated, use `require'bufferline'.setup` instead. " ..
+      'See `:h barbar-setup` for more information.',
+    vim.log.levels.WARN
+  )
+end
+
+require'bufferline'.setup(user_config)
