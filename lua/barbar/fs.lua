310bca725559aae4660ebfdf0c28c02a47e45ef2
Rom Grk <romgrk.cc@gmail.com>
2023-04-02 20:49:40 -0400

    lint

--- lua/barbar/fs.lua
+++ lua/barbar/fs.lua
@@ -8,23 +8,27 @@ local fs = {}
 --- @param filepath string
 --- @param content string
 --- @param mode string
---- @return boolean success
+--- @return string | nil err
 function fs.write(filepath, content, mode)
   mode = mode or 'w'
   local file, open_err = io.open(filepath, mode)
   if open_err ~= nil then
-    return false
+    return open_err
   elseif file == nil then
-    return false
+    return 'Error while opening file'
   end
-  file:write(content)
+  local _, write_err = file:write(content)
+  if write_err ~= nil then
+    return write_err
+  end
+
   local success, close_err = file:close()
   if close_err ~= nil then
-    return false
+    return close_err
   elseif success == false then
-    return false
+    return 'Error while closing file'
   end
-  return true
+  return nil
 end
 
 return fs
