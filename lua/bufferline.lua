8ca29ef4be481535add1d5f8bbd95ddd1388190f
JosefLitos <litosjos@fit.cvut.cz>
2023-11-21 20:02:50 +0100

    refactor: requires on 'bufferline' redirect to 'barbar'

--- lua/bufferline.lua
+++ lua/bufferline.lua
@@ -1,1 +1,10 @@
+-- allow usage of 'bufferline.xxx' instead of 'barbar.xxx'
+-- get the current file path to find all files in `./barbar/`
+-- -15 is the length of `bufferline.lua` from the end of the path
+local dir = debug.getinfo(1, 'S').source:sub(2, -15) .. 'barbar/'
+-- link all lua files in `./barbar/` to `bufferline.xxx`
+for _, f in ipairs(vim.fn.globpath(dir, '*.lua', true, true)) do
+  f = f:sub(#dir + 1, -5)
+  package.preload['bufferline.'..f] = function() return require('barbar.' .. f) end
+end
 return require('barbar')
--- lua/bufferline/render.lua
+++
@@ -1,6 +0,0 @@
-local render = vim.deepcopy(require('barbar.ui.render'))
-
---- @deprecated use `state.restore_buffers` instead
-render.restore_buffers = require('barbar.state').restore_buffers
-
-return render
