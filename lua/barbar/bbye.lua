15db8b6813e26d7d24ae65ecd8023e6463dba368
linrongbin16 <linrongbin16@outlook.com>
2024-05-21 10:52:43 +0800

    fix(bbye): fix switch window failure with 'winbufnr'

--- lua/barbar/bbye.lua
+++ lua/barbar/bbye.lua
@@ -39,6 +39,7 @@ local set_current_buf = vim.api.nvim_set_current_buf --- @type function
 local set_current_win = vim.api.nvim_set_current_win --- @type function
 local win_get_buf = vim.api.nvim_win_get_buf --- @type function
 local win_is_valid = vim.api.nvim_win_is_valid --- @type function
+local winbufnr = vim.fn.winbufnr --- @type function
 
 local state = require('barbar.state')
 local markdown_inline_code = require('barbar.utils').markdown_inline_code
@@ -156,8 +157,8 @@ function bbye.delete(action, force, buffer, mods)
   local wins = list_wins()
   for i = #wins, 1, -1 do
     local window_number = wins[i]
-    if win_is_valid(window_number) and win_get_buf(window_number) == buffer_number then
-      set_current_win(window_number)
+    if win_is_valid(window_number) and winbufnr(i) == buffer_number then
+      command(string.format('execute "%dwincmd w"', i))
 
       -- Bprevious also wraps around the buffer list, if necessary:
       local ok = pcall(function()
@@ -191,6 +192,12 @@ function bbye.delete(action, force, buffer, mods)
   -- Using buflisted() over bufexists() because bufhidden=delete causes the
   -- buffer to still _exist_ even though it won't be :bdelete-able.
   if buflisted(buffer_number) == 1 and buffer_number ~= get_current_buf() then
+    -- Disable auto commands when executing bdelete/bwipeout command
+    if type(mods) == 'string' then
+      mods = mods .. ' noautocmd'
+    elseif type(mods) == 'table' then
+      mods.noautocmd = true
+    end
     local ok, msg = pcall(cmd, action, buffer_number, force, mods)
     if not ok then
       if msg then
